6897af6f6314eeb1e5c95cb6cd176ba254425b02
Sarifor <iread1q84@gmail.com>
2024-11-11 14:27:59 +0900

    Refactor: checkbox sync with storage status, and immediate logic execution
    - Add turn-off feature: update storage status to 'off' when checkbox is unchecked
    - Sync checkbox status and storage status for consistent toggle behavior
    - Separate event listener function to manage add/remove event operation
    - Execute logic immediately after storage status is changed

--- scripts/content.js
+++ scripts/content.js
@@ -1,38 +1,47 @@
-// 확장프로그램 on이면 ㅜ 입력 막고 경고창 띄우기
-// - input 이벤트엔 preventDefault 못 씀
-// - compositionstart: 조합형 문자 입력 시작될 때 발생 for 조합 시작 시 초기화
-//   compositionupdate: 한글 입력 시각 조합이 변화할 때마다 호출 for 조합 중인 문자 실시간 추적
-//   compositionend: 조합이 완료되고 최종 글자가 확정될 때 호출 for 최종 문자 다루기
+// preventU 초기 실행하여, 확장프로그램 on이면 ㅜ 입력 막고 경고창 띄우기
+// - compositionstart 이벤트: 조합형 문자 입력 시작될 때 발생 for 조합 시작 시 초기화
+//   compositionupdate 이벤트: 한글 입력 시각 조합이 변화할 때마다 호출 for 조합 중인 문자 실시간 추적
+//   compositionend 이벤트: 조합이 완료되고 최종 글자가 확정될 때 호출 for 최종 문자 다루기
 // - event.data: 현재 조합된 문자, 읽기 전용
 //   event.target.value: 현재 입력 필드의 전체 값. 읽기/수정 가능
+// - CF) input 이벤트: preventDefault 못 씀
 // - Q. event.key: 영어, 숫자는 잘 나오는데 한글만 유독 Process라고만 나옴 -> event.target.value 쓰기
-// - Q. 입력과 동시에 출력되는 게 아니라 하나씩 늦게 나오는데, 어찌 해결? -> keydown 이벤트 말고 input 이벤트 사용
+// - Q. addEventListener/removeEventListener 제대로 동작시키려면? -> 둘이 정확히 동일한 함수 참조해야 함. 따라서 리스너 함수를 분리시켜 참조
 // - Q. window 대신 document 써도 되나?
-function preventU() {
-  let status;
+function listenerFunc(event) {
+  let compositionData = '';
+  compositionData = event.data;
 
+  if (compositionData === 'ㅜ') {
+    console.log("ㅜ has been entered!");
+    event.target.value = event.target.value.slice(0, - 1);
+    alert("ㅜ is now prohibited!");
+  }
+}
+
+function preventU() {
   chrome.storage.local.get(["status"]).then((result) => {
+    let status;
     status = result.status;
 
     if (status === "on") {
-      console.log("status is on");
-
-      let compositionData = '';
-
-      window.addEventListener('compositionend', (event) => {
-        compositionData = event.data;
-
-        if (compositionData === 'ㅜ') {
-          console.log("ㅜ 입력됨!");
-          event.target.value = event.target.value.slice(0, - 1);
-          alert('ㅜ는 이제 못 써요!');
-        }
-
-      });
-    } else {
-      console.log("status is off");
+      window.addEventListener('compositionend', listenerFunc);
     }
   });
 }
 
 preventU();
+
+// chrome.storage.local.status가 변경될 때, 로직 자동 변경
+// - onChanged.addEventListener (X)
+// - changes.status.newValue 부분 변경 X
+// - Q. newValue === 'on'일 때 곧바로 window.addEventListener 실행시키는 게 더 나을지도?
+chrome.storage.onChanged.addListener((changes) => {
+  if (changes) {
+    if (changes.status.newValue === 'on') {
+      preventU();
+    } else {
+      window.removeEventListener('compositionend', listenerFunc);
+    }
+  }
+});
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,18 +1,36 @@
-// 버튼 값을 크롬 저장소에 보관
-// - manifest.json에서 storage 접근 허가
-// - { status: true } (X)
+// checkbox 가져오기
 const checkbox = document.getElementById('toggleButton');
 
+// chrome.storage.local.status 값을 버튼 상태에 일치시키기
+// - 확장프로그램을 설치하고 처음 실행했을 땐 status 값이 undefined -> 버튼 상태 false
+chrome.storage.local.get('status').then((result) => {
+  if (result.status === 'on') {
+    checkbox.checked = true;
+  } else {
+    checkbox.checked = false;
+  }
+});
+
+// 버튼 상태를 chrome.storage.local.status에 일치시키기
+// - manifest.json에서 storage 접근 허가
+// - chrome.storage.local: 데이터는 로컬에 저장되며 확장 프로그램이 삭제되면 지워짐
+// - { status: true } (X)
 checkbox.addEventListener('change', () => {
   if (checkbox.checked) {
     chrome.storage.local.set({ status: "on"}).then(() => {
-      console.log("status is updated!");
-    })
-
-    chrome.storage.local.get(["status"]).then((result) => {
-      console.log("status is " + result.status);
+      console.log("Status has been updated to 'on'!");
     });
+
+    chrome.storage.local.get(null).then((result) => {
+      console.log("chrome.storage.local 모든 값: ", result);
+    }); // ok
   } else {
-    console.log("OFF!");
+    chrome.storage.local.set({ status: "off"}).then(() => {
+      console.log("Status has been updated to 'off'!");
+    });
+
+    chrome.storage.local.get(null).then((result) => {
+      console.log("chrome.storage.local 모든 값: ", result);
+    });
   }
 });
