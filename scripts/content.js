ebec6a68943aff86c6188636e13eb41abefad18d
Sarifor <iread1q84@gmail.com>
2025-01-29 20:26:00 +0900

    Add: comments

--- scripts/content.js
+++ scripts/content.js
@@ -1,5 +1,6 @@
 // 이벤트 리스너 함수
-// - String의 includes() 메서드는 대소문자 구분함
+// - String의 includes() 메서드는 대소문자를 구별함
+// - event.data(새로 입력된 단일 문자) ≠ event.target.value(입력 필드의 전체 문자열)
 function listenerFunc(event) {
   let inputData = '';
   inputData = event.target.value;
@@ -14,9 +15,8 @@ function listenerFunc(event) {
 // chrome.storage 상태 변경 이벤트를 감지하여, 새 status 값에 맞춰 로직 변경
 // - onChanged.addEventListener (X)
 // - changes.status.newValue 부분 변경 X
-//   CF) input 이벤트: preventDefault 못 씀
+// - input 이벤트: preventDefault 못 씀. window에서 이벤트 인식 못 함
 // - Q. addEventListener/removeEventListener 제대로 동작시키려면? -> 둘이 정확히 동일한 함수 참조해야 함. 따라서 리스너 함수를 분리시켜 참조
-// - Q. window vs. document ?
 chrome.storage.onChanged.addListener((changes) => {
   if (changes) {
     if (changes.status.newValue === 'on') {
